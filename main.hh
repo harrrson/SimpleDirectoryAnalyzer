745d9f8f29203821d626799db96bc0f3e2cc9b89
harrrson <petrakus03+git@gmail.com>
2021-11-14 20:17:42 +0100

    Initial implementation of main and iteration

--- main.hh
+++ main.hh
@@ -1,1 +1,10 @@
+#pragma once
 #include <iostream>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+
+void iterate_through_directory(fs::path const p, int& nof, long& nol);
+long count_lines_in_file(fs::directory_entry const de);
+
